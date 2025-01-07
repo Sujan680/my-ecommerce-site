e553c9fb464a80515225619515f7f1655a2c638e
sujan <budhasujan3@gmail.com>
2025-01-07 18:36:27 +0545

    unauthorized access page

---
+++ client/src/pages/unauth-page/index.jsx
@@ -0,0 +1,14 @@
+
+
+
+import React from 'react';
+
+function UnauthPage() {
+  return (
+    <div>
+      <h1>You don't have access to this page...</h1>
+    </div>
+  );
+}
+
+export default UnauthPage;
